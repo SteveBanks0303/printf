72d4b58026b010a8d362dea4db252389b07d9902
SteveBanks0303 <111028784+SteveBanks0303@users.noreply.github.com>
2022-10-19 01:46:13 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,11 +3,11 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * * _printf - Printf function
- * * @format: format.
- * * Return: Printed chars.
- * * author Stephen Ayomide and Nasirudeen Akinlaso
- * */
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ * author Stephen Ayomide and Nasirudeen Akinlaso
+ */
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
@@ -54,10 +54,10 @@ int _printf(const char *format, ...)
 }
 
 /**
- * * print_buffer - Prints the contents of the buffer if it exist
- * * @buffer: Array of chars
- * * @buff_ind: Index at which to add next char, represents the length.
- * */
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
